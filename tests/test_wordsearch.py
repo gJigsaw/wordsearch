75d075e6d58dc01a776dc69194d840421f0d10e1
topher <topher@findtopher.com>
2019-05-07 08:25:21 -0400

    add test for finding words in a column

--- tests/test_wordsearch.py
+++ tests/test_wordsearch.py
@@ -1,8 +1,26 @@
 """
 Test the wordsearch module
 """
+#pylint: disable=redefined-outer-name
 import pytest
 from wordsearch.wordsearch import WordSearch
+
+
+@pytest.fixture
+def testable_wordsearch():
+    """Create a test matrix to be used in future tests"""
+
+    word_list = ['YUM', 'BOA', 'YEP', 'YAM', 'CBM', 'GUY']
+
+    wordsearch = WordSearch(3, word_list)
+
+    wordsearch.matrix[0] = ['Y', 'U', 'M']
+    wordsearch.matrix[1] = ['A', 'O', 'B']
+    wordsearch.matrix[2] = ['M', 'F', 'C']
+
+    wordsearch.words_found = wordsearch.find_words()
+
+    return wordsearch
 
 def test_new_matrix():
     """Test creating a new matrix and confirm there are no default values remaining"""
@@ -18,18 +36,16 @@ def test_new_matrix():
     assert wordsearch.matrix.shape == (5, 5)
     assert not zeros_exist
 
-def test_finding_words_in_row():
+def test_finding_words_in_row(testable_wordsearch):
     """Test finding words in a row of a given matrix"""
-    word_list = ['YUM', 'BOA', 'YEP']
 
-    wordsearch = WordSearch(3, word_list)
+    assert 'YUM' in testable_wordsearch.words_found
+    assert 'BOA' in testable_wordsearch.words_found
+    assert 'YEP' not in testable_wordsearch.words_found
 
-    wordsearch.matrix[0] = ['Y', 'U', 'M']
-    wordsearch.matrix[1] = ['A', 'O', 'B']
-    wordsearch.matrix[2] = ['M', 'F', 'C']
+def test_finding_words_in_column():
+    """Test finding words in a column of a given matrix"""
 
-    wordsearch.words_found = wordsearch.find_words()
-
-    assert 'YUM' in wordsearch.words_found
-    assert 'BOA' in wordsearch.words_found
-    assert 'YEP' not in wordsearch.words_found
+    assert 'YAM' in testable_wordsearch.words_found
+    assert 'CBM' in testable_wordsearch.words_found
+    assert 'GUY' not in testable_wordsearch.words_found
